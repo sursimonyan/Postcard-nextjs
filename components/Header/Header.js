46e632b39827adecd5e1b7dc3764d1d87c4ebf05
suren-simonyan <suren.simonyan20@gmail.com>
2022-05-28 13:27:14 +0400

    added project to git

---
+++ components/Header/Header.js
@@ -0,0 +1,11 @@
+import styles from '../../styles/Header.module.scss';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            
+        </header>
+    );
+};
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import Layout from '../components/Layout/Layout';
+import '../styles/main-styles/globals.scss';
+import '../styles/main-styles/variables.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
